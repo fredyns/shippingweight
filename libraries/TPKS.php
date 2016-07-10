66a62d2a2ede9a42b58b9b943cecabca0e6d9af2
Fredy Nurman Saleh <email@fredyns.net>
2016-07-10 14:14:05 +0700

    new API

--- libraries/TPKS.php
+++ libraries/TPKS.php
@@ -2,6 +2,8 @@
 
 namespace app\libraries;
 
+use Yii;
+use DateTime;
 use yii\helpers\ArrayHelper;
 use yii\web\HttpException;
 
@@ -12,10 +14,18 @@ use yii\web\HttpException;
  */
 class TPKS
 {
-    const URI_CONTAINERLIST = 'http://api.tpks.co.id/api/api/container_data_ksop_xml';
-    const URI_CONTAINER     = 'http://api.tpks.co.id/api/api/detail_petikemas/container_no';
-    const URI_EMKL          = 'http://api.tpks.co.id/api/api/emkl'; // belum ada
+    const URI_CONTAINERLIST = 'http://api.tpks.co.id/api/api/find_data_vgm_by_time';
+    const URI_CONTAINER     = 'http://api.tpks.co.id/api/api/find_data_vgm_by_container_no/container_no';
+    const URI_CUSTOMERLIST  = 'http://api.tpks.co.id/api/api/master_customer';
+    const URI_CUSTOMER      = 'http://api.tpks.co.id/api/api/find_customer/customer_id';
 
+    /**
+     * prepare uri parameters
+     *
+     * @param string $uri
+     * @param array $params
+     * @return string
+     */
     static function composeUri($uri, $params = [])
     {
         $request = [];
@@ -28,73 +38,155 @@ class TPKS
         return $uri.'?'.implode('&', $request);
     }
 
-    static function curlExec($uri)
+    /**
+     * calling to TPKS server
+     *
+     * @param string $uri
+     * @return array
+     * @throws HttpException
+     */
+    public static function curl($uri)
     {
-        $curl = curl_init();
+        $curl        = curl_init();
+        $credentials = Yii::$app->params['tpks_username'].':'.Yii::$app->params['tpks_password'];
 
         curl_setopt($curl, CURLOPT_URL, $uri);
-        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($curl, CURLOPT_USERPWD, $credentials); //Your credentials goes here
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false); //IMP if the url has https and you don't want to verify source certificate
 
-        $result = curl_exec($curl);
+        $response = curl_exec($curl);
 
         curl_close($curl);
 
-        if (empty($result))
+        if (empty($response))
         {
-            throw new HttpException(404, 'Weighing server is not responding.');
+            throw new HttpException(404, 'TPKS server is not responding.');
         }
 
-        if (strpos($result, 'xml') !== FALSE)
+        if (strpos($response, 'xml') !== FALSE)
         {
-            $xml    = simplexml_load_string($result);
-            $result = json_encode($xml);
+            $xml      = simplexml_load_string($response);
+            $response = json_encode($xml);
         }
 
-        $result = json_decode($result, TRUE);
+        $response = json_decode($response, TRUE);
 
-        if (is_array($result) == FALSE)
+        if (is_array($response) == FALSE)
         {
             throw new HttpException(404, 'Response error.');
         }
 
-        if (isset($result['status']) == FALSE)
+        if (isset($response['status']) == FALSE)
         {
             throw new HttpException(404, 'Response format error.');
         }
 
-        return $result;
-    }
-
-    static function container($number)
-    {
-        // alamat API
-        $uri = static::URI_CONTAINER.'/'.$number;
-
-        // eksekusi
-        $response = static::curlExec($uri);
-
-        // cek status
-        if ($response['status'] == 0)
+        if ($response['status'] == FALSE)
         {
             $message = ArrayHelper::getValue($response, 'msg', 'Data not found.');
 
             throw new HttpException(404, $message);
         }
 
+        return $response;
+    }
+
+    /**
+     * extract named data
+     *
+     * @param string $uri
+     * @param string $name
+     * @param boolean $firstRow
+     * @return array
+     * @throws HttpException
+     */
+    public static function getData($uri, $name, $firstRow = FALSE)
+    {
+        // eksekusi
+        $response = static::curl($uri);
+
         // listing
-        $list = ArrayHelper::getValue($response, 'data_last_detail_container');
+        $list = ArrayHelper::getValue($response, $name);
 
         // cek listing
-        if (!$list OR is_array($list) == FALSE OR count($list) == 0)
+        if (empty($list) OR is_array($list) == FALSE)
         {
             throw new HttpException(404, 'Data empty.');
         }
 
-        // ambil data container yg dimaksud
-        $container = array_shift($list);
+        // returning first row only
+        if ($firstRow)
+        {
+            return array_shift($list);
+        }
 
-        // hasil
-        return $container;
+        // return all dataset
+        return $list;
+    }
+
+    /**
+     * search container weighing at time interval
+     *
+     * @param DateTime $from
+     * @param DateTime $to
+     * @return array
+     * @throws HttpException
+     */
+    public static function containerList(DateTime $from, DateTime $to)
+    {
+        // parameter
+        $param = [
+            'time_from' => $from->format('YmdHi'),
+            'time_to'   => $to->format('YmdHi'),
+        ];
+        $uri   = static::composeUri(static::URI_CONTAINERLIST, $param);
+
+        // get all containers
+        return static::getData($uri, 'data_vgm');
+    }
+
+    /**
+     * search by container number
+     *
+     * @param string $number
+     * @return array
+     * @throws HttpException
+     */
+    public static function container($number)
+    {
+        // alamat API
+        $uri = static::URI_CONTAINER.'/'.$number;
+
+        // get container
+        return static::getData($uri, 'data_vgm', TRUE);
+    }
+
+    /**
+     * get all customer data
+     *
+     * @return array
+     */
+    public static function customerList()
+    {
+        // get all customers
+        return static::getData(static::URI_CUSTOMERLIST, 'data_customer');
+    }
+
+    /**
+     * get customer's data by its ID
+     *
+     * @param string $id
+     * @return array
+     */
+    public static function customer($id)
+    {
+        // alamat API
+        $uri = static::URI_CUSTOMER.'/'.$id;
+
+        // get container
+        return static::getData($uri, 'data_customer', TRUE);
     }
 
 }
