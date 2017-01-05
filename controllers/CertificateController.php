696cbb6371195afd312a9f2a102069a684618bc0
Fredy Nurman Saleh <email@fredyns.net>
2017-01-05 11:09:25 +0700

    overider

--- controllers/CertificateController.php
+++ controllers/CertificateController.php
@@ -29,8 +29,7 @@ class CertificateController extends Controller
 
         $model = Certificate::findOne($id);
 
-        if ($model)
-        {
+        if ($model) {
             header('Content-Type: image/png');
             header("Content-Disposition: inline; filename=vgm-".$model->number.".png;");
 
@@ -50,48 +49,53 @@ class CertificateController extends Controller
      */
     public function actionPdf($id = null, $container_number = null)
     {
-        if (empty($id))
-        {
+        if (empty($id)) {
             return $this->redirect(['pdf-sample', 'container_number' => $container_number]);
         }
 
         $viewing = 0;
-        $id      = str_replace('view', '', $id, $viewing);
-        $model   = Certificate::findOne($id);
+        $id = str_replace('view', '', $id, $viewing);
+        $model = Certificate::findOne($id);
 
-        if (empty($model))
-        {
+        if (empty($model)) {
             throw new NotFoundHttpException("Container not found.");
         }
 
-        if ($viewing)
-        {
+        if ($viewing) {
             return $this->render('pdf', ['model' => $model]);
         }
 
+        $overide = [
+            'container_number' => 'grossmass',
+        ];
+
+        if (array_key_exists($model->number, $overide)) {
+            $model->grossmass = $overide[$model->number];
+        }
+
         $content = $this->renderPartial('pdf', ['model' => $model]);
-        $css     = $this->renderPartial('pdf.css');
+        $css = $this->renderPartial('pdf.css');
 
         \Yii::$app->response->format = \yii\web\Response::FORMAT_RAW;
 
         // setup kartik\mpdf\Pdf component
         $pdf = new Pdf([
-            'options'         => ['title' => 'VGM Certificate by BKI'],
-            'filename'        => 'vgm-'.$model->number.'.pdf',
+            'options' => ['title' => 'VGM Certificate by BKI'],
+            'filename' => 'vgm-'.$model->number.'.pdf',
             // set to use core fonts only
-            'mode'            => Pdf::MODE_CORE,
+            'mode' => Pdf::MODE_CORE,
             // A4 paper format
-            'format'          => Pdf::FORMAT_A4,
+            'format' => Pdf::FORMAT_A4,
             // portrait orientation
-            'orientation'     => Pdf::ORIENT_PORTRAIT,
+            'orientation' => Pdf::ORIENT_PORTRAIT,
             // stream to browser inline
-            'destination'     => Pdf::DEST_BROWSER,
+            'destination' => Pdf::DEST_BROWSER,
             // your html content input
-            'content'         => $content,
-            'cssInline'       => $css,
-            'marginTop'       => 10,
+            'content' => $content,
+            'cssInline' => $css,
+            'marginTop' => 10,
             'defaultFontSize' => 12,
-            'marginFooter'    => 15,
+            'marginFooter' => 15,
         ]);
 
         // return the pdf output as per the destination setting
@@ -100,47 +104,45 @@ class CertificateController extends Controller
 
     public function actionPdfFile($id = null, $container_number = null)
     {
-        $viewing          = 0;
-        $id               = str_replace('view', '', $id, $viewing);
+        $viewing = 0;
+        $id = str_replace('view', '', $id, $viewing);
         $container_number = str_replace('view', '', $container_number, $viewing);
-        $criteria         = $id ? $id : ['number' => $container_number];
-        $model            = Certificate::findOne($criteria);
+        $criteria = $id ? $id : ['number' => $container_number];
+        $model = Certificate::findOne($criteria);
 
-        if (empty($model))
-        {
+        if (empty($model)) {
             return $this->redirect(['pdf-sample', 'container_number' => $container_number]);
         }
 
-        if ($viewing)
-        {
+        if ($viewing) {
             return $this->render('pdf', ['model' => $model]);
         }
 
         $content = $this->renderPartial('pdf', ['model' => $model]);
-        $css     = $this->renderPartial('pdf.css');
-        $folder  = "@web/contents/certificate/{$model->id}";
+        $css = $this->renderPartial('pdf.css');
+        $folder = "@web/contents/certificate/{$model->id}";
 
         \Yii::$app->response->format = \yii\web\Response::FORMAT_RAW;
 
         // setup kartik\mpdf\Pdf component
         $pdf = new Pdf([
-            'options'         => ['title' => 'VGM Certificate by BKI'],
-            'filename'        => $folder.'/vgm-'.$model->number.'.pdf',
+            'options' => ['title' => 'VGM Certificate by BKI'],
+            'filename' => $folder.'/vgm-'.$model->number.'.pdf',
             // set to use core fonts only
-            'mode'            => Pdf::MODE_CORE,
+            'mode' => Pdf::MODE_CORE,
             // A4 paper format
-            'format'          => Pdf::FORMAT_A4,
+            'format' => Pdf::FORMAT_A4,
             // portrait orientation
-            'orientation'     => Pdf::ORIENT_PORTRAIT,
+            'orientation' => Pdf::ORIENT_PORTRAIT,
             // stream to browser inline
-            'destination'     => Pdf::DEST_BROWSER,
+            'destination' => Pdf::DEST_BROWSER,
             // your html content input
-            'content'         => $content,
-            'cssInline'       => $css,
-            'marginTop'       => 10,
+            'content' => $content,
+            'cssInline' => $css,
+            'marginTop' => 10,
             'defaultFontSize' => 12,
-            'marginFooter'    => 15,
-            'destination'     => Pdf::DEST_FILE,
+            'marginFooter' => 15,
+            'destination' => Pdf::DEST_FILE,
         ]);
 
         // return the pdf output as per the destination setting
@@ -149,38 +151,37 @@ class CertificateController extends Controller
 
     public function actionPdfSample($container_number = 0)
     {
-        $viewing          = 0;
+        $viewing = 0;
         $container_number = str_replace('view', '', $container_number, $viewing);
-        $model            = $this->findVGM(null, $container_number);
+        $model = $this->findVGM(null, $container_number);
 
-        if ($viewing)
-        {
+        if ($viewing) {
             return $this->render('pdf-sample', ['model' => $model]);
         }
 
         $content = $this->renderPartial('pdf-sample', ['model' => $model]);
-        $css     = $this->renderPartial('pdf-sample.css');
+        $css = $this->renderPartial('pdf-sample.css');
 
         \Yii::$app->response->format = \yii\web\Response::FORMAT_RAW;
 
         // setup kartik\mpdf\Pdf component
         $pdf = new Pdf([
-            'options'         => ['title' => 'VGM Certificate by BKI'],
-            'filename'        => 'vgm-'.$container_number.'.pdf',
+            'options' => ['title' => 'VGM Certificate by BKI'],
+            'filename' => 'vgm-'.$container_number.'.pdf',
             // set to use core fonts only
-            'mode'            => Pdf::MODE_CORE,
+            'mode' => Pdf::MODE_CORE,
             // A4 paper format
-            'format'          => Pdf::FORMAT_A4,
+            'format' => Pdf::FORMAT_A4,
             // portrait orientation
-            'orientation'     => Pdf::ORIENT_PORTRAIT,
+            'orientation' => Pdf::ORIENT_PORTRAIT,
             // stream to browser inline
-            'destination'     => Pdf::DEST_BROWSER,
+            'destination' => Pdf::DEST_BROWSER,
             // your html content input
-            'content'         => $content,
-            'cssInline'       => $css,
-            'marginTop'       => 10,
+            'content' => $content,
+            'cssInline' => $css,
+            'marginTop' => 10,
             'defaultFontSize' => 12,
-            'marginFooter'    => 15,
+            'marginFooter' => 15,
         ]);
 
         // return the pdf output as per the destination setting
@@ -191,70 +192,59 @@ class CertificateController extends Controller
     {
         $model = null;
 
-        if ($id)
-        {
+        if ($id) {
             $model = Certificate::findOne($id);
-        }
-        elseif ($containerNumber)
-        {
-            try
-            {
-                $vgm       = TPKS::container($containerNumber);
-                $model     = [
-                    'id'             => '0',
-                    'number'         => $containerNumber,
+        } elseif ($containerNumber) {
+            try {
+                $vgm = TPKS::container($containerNumber);
+                $model = [
+                    'id' => '0',
+                    'number' => $containerNumber,
                     'booking_number' => ArrayHelper::getValue(
                         $vgm, 'JOB_ORDER_NO', '<i class="sample-text">booking number</i>'
                     ),
-                    'grossmass'      => '<i class="sample-text">mass</i>',
-                    'weighing_date'  => '<i class="sample-text">date</i>',
-                    'shipper'        => [
-                        'name'    => '<i class="sample-text">shipper name belong here</i>',
+                    'grossmass' => '<i class="sample-text">mass</i>',
+                    'weighing_date' => '<i class="sample-text">date</i>',
+                    'shipper' => [
+                        'name' => '<i class="sample-text">shipper name belong here</i>',
                         'address' => '<i class="sample-text">shipper company belong here</i>',
-                        'email'   => '<i class="sample-text">shipper email belong here</i>',
+                        'email' => '<i class="sample-text">shipper email belong here</i>',
                     ],
                 ];
                 $grossmass = ArrayHelper::getValue($vgm, 'GROSS_KG');
-                $verfTime  = ArrayHelper::getValue($vgm, 'GROSS_VERIFIED_TIME');
+                $verfTime = ArrayHelper::getValue($vgm, 'GROSS_VERIFIED_TIME');
 
-                if ($grossmass)
-                {
+                if ($grossmass) {
                     $model['grossmass'] = number_format($grossmass, 0, '.', ',').' KGM';
                 }
 
-                if ($verfTime)
-                {
+                if ($verfTime) {
                     $date = DateTime::createFromFormat('d-m-Y H:i:s', $verfTime);
 
-                    if ($date)
-                    {
+                    if ($date) {
                         $model['weighing_date'] = $date->format('M d, Y');
                     }
                 }
-            }
-            catch (Exception $exc)
-            {
-
+            } catch (Exception $exc) {
+                
             }
         }
 
-        if (!$model)
-        {
+        if (!$model) {
             $model = [
-                'id'             => '0',
-                'number'         => $containerNumber,
+                'id' => '0',
+                'number' => $containerNumber,
                 'booking_number' => '<i class="sample-text">booking number</i>',
-                'grossmass'      => '<i class="sample-text">mass</i>',
-                'weighing_date'  => '<i class="sample-text">date</i>',
-                'shipper'        => [
-                    'name'    => '<i class="sample-text">shipper name belong here</i>',
+                'grossmass' => '<i class="sample-text">mass</i>',
+                'weighing_date' => '<i class="sample-text">date</i>',
+                'shipper' => [
+                    'name' => '<i class="sample-text">shipper name belong here</i>',
                     'address' => '<i class="sample-text">shipper company belong here</i>',
-                    'email'   => '<i class="sample-text">shipper email belong here</i>',
+                    'email' => '<i class="sample-text">shipper email belong here</i>',
                 ],
             ];
         }
 
         return $model;
     }
-
 }
